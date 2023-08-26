3bcc1cdb8efbc481962b5055b48c9289a79a1206
Yash Ahuja <Yashahujaofficial@gmail.com>
2023-08-26 23:56:32 +0530

    firebase updated configurations

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,12 +1,12 @@
 export const environment = {
-    firebase: {
-        production: false,
-        apiKey: "AIzaSyBrkT6Gqs8kqnqT9nBahOMaNd_cXf9YsAQ",
-        authDomain: "interactive-map-d361d.firebaseapp.com",
-        projectId: "interactive-map-d361d",
-        storageBucket: "interactive-map-d361d.appspot.com",
-        messagingSenderId: "770149799781",
-        appId: "1:770149799781:web:308b82ef6eb07edb16738c",
-        measurementId: "G-Z2D92SZFGR"
-      }
+  firebase: {
+    production: false,
+    apiKey: 'AIzaSyBDFDlfZkXhFhTK-9-dgCuEb0zEuPbTXdM',
+    authDomain: 'interactive-map-e7908.firebaseapp.com',
+    projectId: 'interactive-map-e7908',
+    storageBucket: 'interactive-map-e7908.appspot.com',
+    messagingSenderId: '185676807819',
+    appId: '1:185676807819:web:da8cc5631a1437f7cb9e8e',
+    measurementId: 'G-T6EYVDGC0H',
+  },
 };
